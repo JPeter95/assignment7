7997fa306ccd072d7648d86197bdd8d431dca194
Joseph Peterson <joseph.peterson@jhuapl.edu>
2020-08-09 22:44:50 -0400

    Intial push to setup sessions/authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
   root :to => 'users#index'
   resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get '/users' => "users#new", as: "users_list"
+  get '/login' => "sessions#new", as: "login"
+  delete '/logout' => "sessions#destroy", as: "logout"
 end
