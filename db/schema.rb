d4daf8fb65ee56dadcb6a3fc69c5286ed97378ce
Joseph Peterson <joseph.peterson@jhuapl.edu>
2020-08-10 00:10:44 -0400

    Added done field to todo_items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_025447) do
+ActiveRecord::Schema.define(version: 2020_08_10_040025) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "gender"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_025447) do
     t.integer "todo_list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "done"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
