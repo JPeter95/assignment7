d07bfd9d8b3396f2a0a9f6d5430cb7572cd70239
Joseph Peterson <joseph.peterson@jhuapl.edu>
2020-08-09 21:48:25 -0400

    Implemented secure p/w for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	has_secure_password
+	
 	has_one :account, dependent: :destroy
 	has_many :todo_lists, dependent: :destroy
 	has_many :things_todo, through: :todo_lists, source: :todo_items
